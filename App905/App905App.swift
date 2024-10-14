9c62f35bd11e89a05e13a76689aee2afe30deadc
IGOLIVIT <131347905+IGOLIVIT@users.noreply.github.com>
2024-10-14 22:16:08 +0300

    Initial Commit

---
+++ App905/App905App.swift
@@ -0,0 +1,17 @@
+//
+//  App905App.swift
+//  App905
+//
+//  Created by IGOR on 14/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct App905App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
